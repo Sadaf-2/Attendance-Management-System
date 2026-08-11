64f654c52276faf603f6cbf81b31a1f05d276181
sadaf iqbal <isadaf240@gmail.com>
2026-08-11 14:18:20 +0500

    Fix backend CORS

--- backend/flask/app.py
+++ backend/flask/app.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
-
 from config import Config
 from database import db
 from routes import auth
@@ -14,18 +13,25 @@ db.init_app(app)
 
 bcrypt = Bcrypt(app)
 
-CORS(app)
+CORS(
+    app,
+    resources={r"/*": {"origins": "*"}},
+    methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_headers=["Content-Type", "Authorization"]
+)
 
 app.register_blueprint(auth)
 
 with app.app_context():
     db.create_all()
 
+
 @app.route("/")
 def home():
     return {
         "message": "Attendance Management System API is Running"
     }
+
 
 if __name__ == "__main__":
     app.run(debug=True)
